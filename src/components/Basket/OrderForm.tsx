e0509568036d01e99c8ceaa437891cac58a1d2c3
Kapustin Stepan <s.kapustin@webpractik.ru>
2022-07-19 15:26:06 +0300

    feat(react-hook-form): подключил валидацию

--- src/components/Basket/OrderForm.tsx
+++ src/components/Basket/OrderForm.tsx
@@ -5,6 +5,8 @@ type Inputs = {
     name: string;
     phone: string;
     adress: string;
+    radio: string;
+    online: string;
 };
 
 const OrderForm = () => {
@@ -14,6 +16,12 @@ const OrderForm = () => {
         watch,
         formState: { errors },
     } = useForm<Inputs>();
+
+    const name = watch('name');
+    const phone = watch('phone');
+    const adress = watch('adress');
+    const radio = watch('radio');
+
     const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
     return (
         <form
@@ -25,7 +33,7 @@ const OrderForm = () => {
         >
             <span className="buy-form__title">Контакты</span>
             <div className="buy-form__wrap buy-form__wrap--two--сolumn">
-                <div className="buy-form__wrap--input">
+                <div className={name ? 'buy-form__wrap--input complete' : 'buy-form__wrap--input'}>
                     <input
                         type="text"
                         className={errors.name ? 'buy-form__input-error' : 'buy-form__input'}
@@ -37,13 +45,17 @@ const OrderForm = () => {
                         Ваше имя
                     </label>
                 </div>
-                <div className="buy-form__wrap--input">
+                <div className={phone ? 'buy-form__wrap--input complete' : 'buy-form__wrap--input'}>
                     <input
                         type="text"
                         className={errors.phone ? 'buy-form__input-error' : 'buy-form__input'}
                         name="phone"
                         id="phone"
-                        {...register('phone', { required: true, maxLength: 18 })}
+                        {...register('phone', {
+                            required: true,
+                            maxLength: 18,
+                            pattern: /^([+]?[0-9\s-\(\)]{3,25})*$/i,
+                        })}
                     />
                     <label className="buy-form__label" htmlFor="phone">
                         Телефон
@@ -51,7 +63,9 @@ const OrderForm = () => {
                 </div>
             </div>
             <div className="buy-form__wrap">
-                <div className="buy-form__wrap--input">
+                <div
+                    className={adress ? 'buy-form__wrap--input complete' : 'buy-form__wrap--input'}
+                >
                     <input
                         type="text"
                         className={errors.adress ? 'buy-form__input-error' : 'buy-form__input'}
@@ -71,21 +85,70 @@ const OrderForm = () => {
                     className="buy-form__radio"
                     name="payment"
                     id="paymentСourier"
+                    {...register('radio', { required: true })}
+                    value="paymentСourier"
                 />
                 <label htmlFor="paymentСourier" className="buy-form__radio--label">
                     Оплата наличными или картой курьеру
                 </label>
             </div>
             <div className="buy-from__radio">
-                <input type="radio" className="buy-form__radio" name="payment" id="paymentSite" />
+                <input
+                    type="radio"
+                    className="buy-form__radio"
+                    name="payment"
+                    id="paymentSite"
+                    {...register('radio', { required: true })}
+                    value="paymentSite"
+                />
                 <label htmlFor="paymentSite" className="buy-form__radio--label">
                     Оплата картой онлайн на&nbsp;сайте
                 </label>
             </div>
-            <label htmlFor="" className="error">
-                Выберите способ оплаты!
-            </label>
-            <button className="buy-form__btn--buy" type="button" tabIndex={0}>
+            {radio === 'paymentSite' ? (
+                <>
+                    <div className="buy-from__radio">
+                        <input
+                            type="radio"
+                            className="buy-form__radio"
+                            name="visa"
+                            id="visa"
+                            {...register('online', { required: true })}
+                            value="visa"
+                        />
+                        <label htmlFor="visa" className="buy-form__radio--label">
+                            Visa
+                        </label>
+                    </div>
+                    <div className="buy-from__radio">
+                        <input
+                            type="radio"
+                            className="buy-form__radio"
+                            name="master"
+                            id="master"
+                            {...register('online', { required: true })}
+                            value="master"
+                        />
+                        <label htmlFor="master" className="buy-form__radio--label">
+                            Mastercard
+                        </label>
+                    </div>
+                    {errors.online && (
+                        <label htmlFor="" className="error">
+                            Выберите способ оплаты!
+                        </label>
+                    )}
+                </>
+            ) : (
+                ''
+            )}
+            {errors.radio && (
+                <label htmlFor="" className="error">
+                    Выберите способ оплаты!
+                </label>
+            )}
+
+            <button className="buy-form__btn--buy" type="submit" tabIndex={0}>
                 Оформить заказ
             </button>
             <div className="buy-form__info smallText">
